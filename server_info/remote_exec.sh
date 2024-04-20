2e1aed628d7bcd280d8c935e8414fde38a35722b
Mihailo222 <mahovacmihailo@gmail.com>
2024-04-20 10:33:32 +0200

    adding a script for system info remote execution on multiple hosts

---
+++ server_info/remote_exec.sh
@@ -0,0 +1,6 @@
+for server in  $( cat hosts.txt ) #assigning each line to a variable server
+do
+ echo "${server}"
+ ssh ubuntu@${server} 'bash -s' < ./server_info.sh
+ #ssh into machine and start new shell instance; read commands from stding (bash -s) and execute script I gave you as stdin 
+done
