fdc27a9cfe66581fcd83fde0882184e516db01c2
Mihailo222 <mahovacmihailo@gmail.com>
2024-04-20 10:35:40 +0200

    automating user

--- server_info/remote_exec.sh
+++ server_info/remote_exec.sh
@@ -1,6 +1,8 @@
+user= "ubuntu"
+
 for server in  $( cat hosts.txt ) #assigning each line to a variable server
 do
  echo "${server}"
- ssh ubuntu@${server} 'bash -s' < ./server_info.sh
+ ssh user@${server} 'bash -s' < ./server_info.sh
  #ssh into machine and start new shell instance; read commands from stding (bash -s) and execute script I gave you as stdin 
 done
